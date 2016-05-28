bf9c9773a383a277fdf30332666d54e9ba8cc9a5
John Larsen <jr.larsen@mac.com>
2016-05-28 16:02:31 +0100

    fix typo

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ var apiRouter = require('./api');
 app.use(express.static(path.resolve(__dirname, "public")));
 app.use('/api', apiRouter);
 
-app.listen(process.env.port || 1337);
+app.listen(process.env.PORT || 1337);
